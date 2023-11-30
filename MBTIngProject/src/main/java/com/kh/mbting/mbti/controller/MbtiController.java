2753cb2f70715d94504fee82a5a6b18b986d7bf7
김선아 <1331349@naver.com>
2023-11-30 18:18:20 +0900

    MBTI 매칭 프론트 구현 + 백엔드 매칭 폴더 생성 (23.11.30) - 김선아
    mbti 매칭 프론트
    매칭 controller, service, dao, vo 생성

--- MBTIngProject/src/main/java/com/kh/mbting/mbti/controller/MbtiController.java
+++ MBTIngProject/src/main/java/com/kh/mbting/mbti/controller/MbtiController.java
@@ -1,18 +1,17 @@
-package com.kh.mbting.mbtiTest.controller;
+package com.kh.mbting.mbti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kh.mbting.mbtiTest.model.service.MbtiTestService;
-
-// 2023.11.26 김선아
+import com.kh.mbting.mbti.model.service.MbtiService;
 
 @Controller
-public class MbtiTestController {
+public class MbtiController {
 	
+		
 	@Autowired	
-	private MbtiTestService mbtiTestService; 
+	private MbtiService mbtiService; 
 	
 //	연애 테스트 클릭 시 테스트 화면으로 넘어가기 위한 메소드
 	@RequestMapping("mbtiTest.mb")
@@ -20,5 +19,15 @@ public class MbtiTestController {
 		
 		return "mbti/mbtiTest";
 	}
+	
+//	테스트 후 매칭 버튼 클릭 시 매칭 화면으로 넘어가기 위한 메소드
+	@RequestMapping("mbtiMatch.mb")
+	public String mbtiMatch() {
+		
+		return "mbti/mbtiMatch";
+	}
+
+
+	
 
 }
