3f36a5eb48e1d4ddd37bd13ea8862fc745b1d639
mingMy-00 <149574203+mingMy-00@users.noreply.github.com>
2023-12-05 00:46:15 +0900

    doa 병합

--- MBTIngProject/src/main/java/com/kh/mbting/chatting/model/dao/ChattingDao.java
+++ MBTIngProject/src/main/java/com/kh/mbting/chatting/model/dao/ChattingDao.java
@@ -1,12 +1,41 @@
 package com.kh.mbting.chatting.model.dao;
 
-import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.kh.mbting.chatting.model.vo.ChatMessage;
+import com.kh.mbting.chatting.model.vo.ChatRoom;
 
 @Repository
 public class ChattingDao {
 	
+	@Autowired
+    SqlSessionTemplate sqlSession;
 
+    public int createChat(ChatRoom room) {
+        return sqlSession.insert("chatMapper.createChat", room);
+    }
+
+    public ChatRoom searchChatRoom(ChatRoom room) {
+        return sqlSession.selectOne("chatMapper.searchChatRoom", room);
+    }
+    
+    public List<ChatRoom> chatRoomList(String userEmail) {
+        return sqlSession.selectList("chatMapper.chatRoomList", userEmail);
+    }
+    
+    public ChatRoom chatRoom(ChatRoom room) {
+    	return sqlSession.selectOne("chatMapper.chatRoom", room);
+    }
+    
+    public ChatRoom selectChatRoom(String roomNo) {
+        return sqlSession.selectOne("chatMapper.selectChatRoom", roomNo);
+    }
+    
+    public int insertMessage(ChatMessage chatMessage) {
+        return sqlSession.insert("chatMapper.insertMessage", chatMessage);
+    }
 }
