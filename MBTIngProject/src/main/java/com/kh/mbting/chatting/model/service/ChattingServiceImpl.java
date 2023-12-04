7f3262de115d3bf99e2f298bc266003d37aa89b6
mingMy-00 <149574203+mingMy-00@users.noreply.github.com>
2023-12-05 00:47:28 +0900

    serviceImpl 병합

--- MBTIngProject/src/main/java/com/kh/mbting/chatting/model/service/ChattingServiceImpl.java
+++ MBTIngProject/src/main/java/com/kh/mbting/chatting/model/service/ChattingServiceImpl.java
@@ -1,12 +1,14 @@
 package com.kh.mbting.chatting.model.service;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.mbting.chatting.model.dao.ChattingDao;
+import com.kh.mbting.chatting.model.vo.ChatMessage;
+import com.kh.mbting.chatting.model.vo.ChatRoom;
 
 @Service
 public class ChattingServiceImpl implements ChattingService{
@@ -17,5 +19,35 @@ public class ChattingServiceImpl implements ChattingService{
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	@Override
+	public int createChat(ChatRoom room) {
+		return chattingDao.createChat(room);
+	}
+
+	@Override
+	public ChatRoom searchChatRoom(ChatRoom room) {
+		return chattingDao.searchChatRoom(room);
+	}
+
+	@Override
+	public List<ChatRoom> chatRoomList(String userEmail) {
+		return chattingDao.chatRoomList(userEmail);
+	}
+
+	@Override
+	public ChatRoom chatRoom(ChatRoom room) {
+		return chattingDao.chatRoom(room);
+	}
+
+	@Override
+	public ChatRoom selectChatRoom(String roomNo) {
+		return chattingDao.selectChatRoom(roomNo);
+	}
+
+	@Override
+	public int insertMessage(ChatMessage chatMessage) {
+		return chattingDao.insertMessage(chatMessage);
+	}
 
 }
